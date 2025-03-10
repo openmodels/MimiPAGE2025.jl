60fa310d3de29bf3ec05b8fd6dc8b3c1ab918e90
James Rising <jarising@gmail.com>
2025-03-10 11:22:07 -0400

    new checks

--- analysis/capital/tests.jl
+++ analysis/capital/tests.jl
@@ -4,7 +4,7 @@ include("../../src/main_model.jl")
 # Test 2: Using savings rate everywhere looks okay?
 # Test 3: Adding in feedback looks okay?
 
-model = getpage()
+model = getpage(config_capital="full")
 run(model)
 
 df = getdataframe(model, :Capital, :fracloss)
@@ -13,5 +13,5 @@ df[(df.time .== 2100) .& (df.fracloss .> .1), :]
 
 df = getdataframe(model, :Capital, :capital_baseline)
 quantile(df.capital_baseline[df.time .== 2100] ./ df.capital_baseline[df.time .== 2020], [0, .25, .75, 1])
-findfirst(df.capital_baseline[df.time .== 2100] ./ df.capital_baseline[df.time .== 2020] .> 50)
-df[df.time .== 2100, :][58, :]
+findfirst(df.capital_baseline[df.time .== 2100] ./ df.capital_baseline[df.time .== 2020] .> 20)
+df[df.time .== 2100, :][19, :]
