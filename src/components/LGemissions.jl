436d14af09a66a6ef96f20523dcea8dac8d3895c
James Rising <jarising@gmail.com>
2025-05-27 10:43:50 -0400

    Moves Kaye-based feedback into core model

--- src/components/LGemissions.jl
+++ src/components/LGemissions.jl
@@ -11,11 +11,21 @@
     # growth rate by region
     er_LGemissionsgrowth = Parameter(index=[time,region], unit="%")
 
+    # read in counterfactual GDP in absence of growth effects (gdp_leveleffects) and actual GDP
+    gdp_region = Parameter(index=[time, region], unit="\$M")
+    gdp_leveleffect_region = Parameter(index=[time, region], unit="\$M")
+    emfeed_emissionfeedback = Parameter{Bool}(unit="none", default=true)
+
     function run_timestep(p, v, d, t)
 
         # eq.4 in Hope (2006) - regional LG emissions as % change from baseline
         for r in d.region
             v.e_regionalLGemissions[t,r] = p.er_LGemissionsgrowth[t,r] * p.e0_baselineLGemissions[r] / 100
+
+            # rescale emissions based on GDP deviation from original scenario pathway
+            if p.emfeed_emissionfeedback
+                v.e_countryCO2emissions[t, r] = v.e_countryCO2emissions[t, r] * (p.gdp_region[t, r] / p.gdp_leveleffect_region[t, r])
+            end
         end
 
         # eq. 5 in Hope (2006) - global LG emissions are sum of regional emissions
