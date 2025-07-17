0fab7a8c38184122c22e1aeeea8254a52c39517a
James Rising <jarising@gmail.com>
2025-07-17 08:24:41 -0400

    add pm model

--- src/components/PMMarketDamages.jl
+++ src/components/PMMarketDamages.jl
@@ -8,7 +8,7 @@
     ## 20µg/m3 in Eastern Europe and other regions (soft upper-bound of OECD PM)
     ## But Largest effect on agricultural sector (outdoor labor) so expect to apply to poor regions
 
-    pm2lngdp = Parameter(default=-0.0116, unit="log-point / µg/m3") # SE = 0.0021, Main text Table 3
+    pm2lngdp = Parameter(default=-0.00545, unit="log-point / µg/m3") # SE = 0.00156, Main text Table 5
     lnpm2lngdp = Parameter(default=-0.1175, unit="log-point / log-point") # SE = 0.0199, Appendix B.3.9 Table 24
 
     dlngdp = Variable(index=[time,country], unit="log-point")
@@ -22,7 +22,13 @@
             lag_pm_total_clip = min.(20., pp.pm_total[tt-1, :])
             value_lin = pp.pm2lngdp * (pm_total_clip - lag_pm_total_clip)
             value_log = pp.pm2lngdp * (log.(pm_total_clip) - log.(lag_pm_total_clip))
-            vv.dlngdp[tt, :] = min.(value_lin, value_log)
+            for cc in dd.country
+                if value_lin > 0
+                    vv.dlngdp[tt, cc] = min(value_lin[cc], value_log[cc])
+                else
+                    vv.dlngdp[tt, cc] = max(value_lin[cc], value_log[cc])
+                end
+            end
         end
     end
 end
